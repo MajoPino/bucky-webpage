40cb512f9f53e1ce400b9f514c3b187fd2067b98
MajoPino <majo12pino@gmail.com>
2024-12-02 08:40:38 -0500

    fix: The file vite config was created

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+export default {
+    base: '/bucky-webpage/', // Reemplaza <REPO-NAME> con el nombre de tu repositorio
+    build: {
+        outDir: 'dist', // Carpeta de salida para el build
+    },
+};
